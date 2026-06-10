6f0ff4ca2d8e75d52c3a4348f6711ff0b8fce036
kailash anshu kumar roy <anshuroy22021409@gmail.com>
2026-06-10 22:24:39 +0530

    basics.py file is changed

--- basics.py
+++ basics.py
@@ -45,6 +45,7 @@ x = [1, 2]
 y = x
 print(x is y)          # Identity
 
+print(50*'*')
 # -----------------------------------------------------------
 # data types
 
@@ -84,3 +85,33 @@ i = None        # NoneType
 
 print(type(a))  # <class 'int'>
 print(type(c))  # <class 'str'>
+
+print(50*'*')
+# -----------------------------------------------------------
+# strings
+# ----------
+# string is a sequence of characters enclosed in single quotes (' '), double quotes (" "), or triple quotes (''' ''' or """ """).
+# strings are immutable sequences of characters so whatever we do with the string it will always return a new string and the original string will remain unchanged
+name = "Anshu Kumar Roy"
+print(name[0])    # A
+print(name[-2])    #o
+print(name[6:11]) # Kumar
+print(name[-3:])  # Roy
+print(len(name))   # 17
+print(name.upper())  # ANSHU KUMAR ROY
+print(name.lower())  # anshu kumar roy
+print(name.split())  # ['Anshu', 'Kumar', 'Roy']
+print(name.replace("Anshu", "Anshul"))  # Anshul Kumar Roy
+print(name.find("Kumar"))  # 6
+print(name.startswith("Anshu"))  # True
+print(name.endswith("Roy"))  # True
+print(name.count("a"))  # 2
+print(name.isalpha())  # False (because of spaces)
+print(name.isalnum())  # False (because of spaces)
+print(name.strip())  # Anshu Kumar Roy (removes leading and trailing whitespace)
+print(name.center(30, "*"))  # *****Anshu Kumar Roy*****
+print('reversing the string:',name[::-1])  # yoR ramuK uhnsA (reverses the string)
+print(name[::2])  # AsuKmrRy (every second character)
+
+print(name+2)  # TypeError: can only concatenate str (not "int") to str
+print(name +str(2))  # Anshu Kumar Roy2 (concatenating string with number)
